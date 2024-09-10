524f3c24b4a167aaccb532c4944ca68f4c20e386
Deepak <deepak72855@gmail,com>
2024-09-10 08:04:28 +0530

    Initial commit

---
+++ exersize-1.py
@@ -0,0 +1,6 @@
+list1= [1,5,6,8,7]
+if 9 not in list1:
+    list1.append(9)
+    print(" it is now in the list")
+else:
+    print("No, it is in the list")
---
+++ exersize-2.py
@@ -0,0 +1,8 @@
+print("enter you age")
+age= int(input())
+if 60>age>18:
+    print("you can drive")
+elif age==18:
+    print("you need to give a drive test")
+else:
+    print("not eligibile for driving")
