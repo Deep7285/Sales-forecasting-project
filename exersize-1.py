18f52a8be99944bb9324c9b8a1c28f7694ec6007
Deepak <deepak72855@gmail,com>
2024-09-10 09:56:45 +0530

    Fixed the message repetition in exersize-1.py

--- exersize-1.py
+++ exersize-1.py
@@ -3,4 +3,6 @@ if 9 not in list1:
     list1.append(9)
     print(" it is now in the list")
 else:
-    print("No, it is in the list")
+    print("No, it in this list")
+    print("No, it in this list")
+    #added here
