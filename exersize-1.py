570778ef4ed3d7b0c96ecab91798e72414754e33
Deepak <114175297+Deep7285@users.noreply.github.com>
2025-02-22 18:29:31 +0530

    Delete exersize-1.py

--- exersize-1.py
+++
@@ -1,8 +0,0 @@
-list1= [1,5,6,8,7]
-if 9 not in list1:
-    list1.append(9)
-    print(" it is now in the list")
-else:
-    print("No, it in this list")
-    print("No, it in this list")
-    #added here
